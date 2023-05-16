2538baad0417a27001ce58ffed661b9b5e1e3e19
Sarah Andersen <119450768+sarah-andersen@users.noreply.github.com>
2023-05-15 21:28:39 -0400

    css improvements

--- main.js
+++ main.js
@@ -2,18 +2,22 @@
 let width = window.innerWidth
 let svgholder = document.getElementById("svgholder");
 
+
 // create counter like let n=0
-// create empty list in which you'll put ids of stuff you click
+// create empty list in which I'll put ids of stuff I have clicked on
 
 svgholder.addEventListener('load', function()
 {
     let circles = svgholder.contentDocument.querySelectorAll('circle')
     circles.forEach(element => {
-        // every time you click, you add to the list the id of teh thing you clicked
-        // check if element id isin list of ids
+        // every time I click, I add to the list the id of the thing I clicked
+        // check if element id is in list of ids
             // if not, counter + 1
                 // document getElement by Id .text - > counter
             // if it is counter doesn't move
+            //Set
+
+
   element.style.fill='transparent'
   element.style.strokeColor = 'transparent'
   element.addEventListener("click", function(){
