54cab4160e6fd61abdf78de197c5a5db203b76b3
Sarah Andersen <119450768+sarah-andersen@users.noreply.github.com>
2023-05-17 22:52:55 -0400

    adding a counter

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 
-let width = window.innerWidth
+// let width = window.innerWidth
 let svgholder = document.getElementById("svgholder");
 
+//counter variable
+let CounterObject = document.getElementById("num-changing")
+let CounterValue = 0
 
-// create counter like let n=0
 // create empty list in which I'll put ids of stuff I have clicked on
 
 svgholder.addEventListener('load', function()
 {
     let circles = svgholder.contentDocument.querySelectorAll('circle')
     circles.forEach(element => {
-        // every time I click, I add to the list the id of the thing I clicked
-        // check if element id is in list of ids
-            // if not, counter + 1
-                // document getElement by Id .text - > counter
-            // if it is counter doesn't move
-            //Set
-
 
   element.style.fill='transparent'
   element.style.strokeColor = 'transparent'
   element.addEventListener("click", function(){
     this.style.stroke = 'red'
     this.style.strokeWidth = '5px'
+    CounterValue = CounterValue+1
+    console.log(CounterValue)
+    CounterObject.textContent = CounterValue
 
+// every time I click, I add to the list the id of the thing I clicked
+        // check if element id is in list of ids
+            // if not, counter + 1
+                // document getElement by Id .text - > counter
+            // if it is counter doesn't move
+            //Set
   });  
 });
 // end if counter == 15
